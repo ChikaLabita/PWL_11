0f7f7ed065c7468bba8109dcaed4d01569eed687
= <=>
2021-04-20 17:53:05 +0700

    "PWL-11 : Praktikum 2 -> Ubah file Handler.php"

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,40 +2,52 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    public function render($request, $exception){
+        if ($exception instanceof ModelNotFoundException && $request->wantsJson()) {
+            return response()->json(
+                ['message' => 'Not Found!'],
+                Response::HTTP_NOT_FOUND
+            );
+        }
+
+        return parent::render($request, $exception);
+    }
     /**
      * A list of the exception types that are not reported.
      *
      * @var array
      */
-    protected $dontReport = [
+    //protected $dontReport = [
         //
-    ];
+    //];
 
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
      * @var array
      */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
+    //protected $dontFlash = [
+        //'current_password',
+        //'password',
+        //'password_confirmation',
+    //];
 
     /**
      * Register the exception handling callbacks for the application.
      *
      * @return void
      */
-    public function register()
-    {
-        $this->reportable(function (Throwable $e) {
+    //public function register()
+    //{
+        //$this->reportable(function (Throwable $e) {
             //
-        });
-    }
+        //});
+    //}
 }
